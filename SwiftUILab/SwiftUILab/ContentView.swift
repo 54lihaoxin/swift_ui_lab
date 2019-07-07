aca9d31e4683ef5e55708b91916381a2c9dc46ab
Haoxin Li <54lihaoxin@gmail.com>
2019-07-07 16:00:27 -0700

    refactor by extracting cell subview

--- SwiftUILab/SwiftUILab/ContentView.swift
+++ SwiftUILab/SwiftUILab/ContentView.swift
@@ -13,16 +13,7 @@ struct ContentView : View {
     var body: some View {
         NavigationView {
             List(rooms) { room in
-                NavigationButton(destination: Text(room.name)) {
-                    Image(room.thumbnailName)
-                        .cornerRadius(8)
-                    VStack(alignment: .leading) {
-                        Text(room.name)
-                        Text("\(room.capacity) people")
-                            .font(.subheadline)
-                            .foregroundColor(.secondary)
-                    }
-                }
+                RoomCell(room: room)
             }
             .navigationBarTitle(Text("Rooms"))
         }
@@ -36,3 +27,19 @@ struct ContentView_Previews : PreviewProvider {
     }
 }
 #endif
+
+struct RoomCell : View {
+    let room: Room
+    var body: some View {
+        return NavigationButton(destination: Text(room.name)) {
+            Image(room.thumbnailName)
+                .cornerRadius(8)
+            VStack(alignment: .leading) {
+                Text(room.name)
+                Text("\(room.capacity) people")
+                    .font(.subheadline)
+                    .foregroundColor(.secondary)
+            }
+        }
+    }
+}
