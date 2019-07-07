7992fb1f9e98a142a6448fa62ce5aa6ab667c188
Haoxin Li <54lihaoxin@gmail.com>
2019-07-07 12:07:22 -0700

    Create brand new SwiftUI project with Xcode 11 beta 2

---
+++ SwiftUILab/SwiftUILab/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwiftUILab
+//
+//  Created by Haoxin Li on 7/7/19.
+//  Copyright © 2019 Haoxin Li. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
