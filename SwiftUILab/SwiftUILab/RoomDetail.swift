59b673342edaaab0d3cb777f3a172b42dedd33e3
Haoxin Li <54lihaoxin@gmail.com>
2019-07-07 16:03:30 -0700

    add `RoomDetail` file

---
+++ SwiftUILab/SwiftUILab/RoomDetail.swift
@@ -0,0 +1,23 @@
+//
+//  RoomDetail.swift
+//  SwiftUILab
+//
+//  Created by Haoxin Li on 7/7/19.
+//  Copyright © 2019 Haoxin Li. All rights reserved.
+//
+
+import SwiftUI
+
+struct RoomDetail : View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#if DEBUG
+struct RoomDetail_Previews : PreviewProvider {
+    static var previews: some View {
+        RoomDetail()
+    }
+}
+#endif
